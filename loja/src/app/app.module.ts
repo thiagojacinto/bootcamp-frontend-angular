5df40fbe92dfba8543ddd11d005ba6ab8b33a810
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-12-21 23:33:09 -0300

    chore: config e import do componente ItemProduto

--- loja/src/app/app.module.ts
+++ loja/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemProdutoComponent } from './produto/item-produto/item-produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemProdutoComponent
   ],
   imports: [
     BrowserModule,
