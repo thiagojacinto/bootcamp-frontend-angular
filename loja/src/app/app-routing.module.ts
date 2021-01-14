983d5ecf8eb3a1a4eead1ef413aa801c8d3153e2
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-01-13 22:47:00 -0300

    chore: config inicial de rotas da applicação

--- loja/src/app/app-routing.module.ts
+++ loja/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemProdutoComponent } from './produto/item-produto/item-produto.component';
+import { ListaProdutosComponent } from './produto/lista-produtos/lista-produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "produto/:id",
+    component: ItemProdutoComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: "produto",
+    component: ListaProdutosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
