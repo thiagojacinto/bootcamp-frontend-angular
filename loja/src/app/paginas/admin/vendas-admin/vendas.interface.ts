9cdcec4e3542977d00d4365f932f46c9ec94034f
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-01-20 21:28:08 -0300

    fix: admin > vendas > typo na interface Items > Itens

--- loja/src/app/paginas/admin/vendas-admin/vendas.interface.ts
+++ loja/src/app/paginas/admin/vendas-admin/vendas.interface.ts
@@ -1,18 +1,18 @@
 export interface Venda {
-  id?: number,
-  datahora: Date,
-  valorTotal: number,
+  id?: number;
+  datahora: Date;
+  valorTotal: number;
   cliente?: {
-    id?: number,
-    nome: string,
-    cpf: string,
-    email?: string
-  },
+    id?: number;
+    nome: string;
+    cpf: string;
+    email?: string;
+  };
   formaPagamento?: {
-    id: number,
-    forma: string,
-    descricao?: string,
-    ativo?: boolean
-  },
-  items: Array<Object>,
+    id: number;
+    forma: string;
+    descricao?: string;
+    ativo?: boolean;
+  };
+  itens: Array<Object>;
 }
