3e67c153c7ba8197480962e9e4bcd099a282f7f7
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-01-22 00:29:33 -0300

    hotfix: link para área ADMIN pelo Footer

--- loja/src/app/compartilhado/componentes/footer/footer.module.ts
+++ loja/src/app/compartilhado/componentes/footer/footer.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { FooterComponent } from './footer.component';
 
 @NgModule({
   declarations: [FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [FooterComponent]
 })
