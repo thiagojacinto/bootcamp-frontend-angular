01ec0bc43931c41de2ac9984c3a8c83e0e2e0a38
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-21 23:18:05 -0300

    feat: restruture rxjs forkJoin usage

--- loja/src/app/paginas/confirmar-compra/confirmar-compra.service.ts
+++ loja/src/app/paginas/confirmar-compra/confirmar-compra.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { forkJoin, Observable, Subject } from 'rxjs';
+import { forkJoin, zip, Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators'
 import { Produto } from 'src/app/produto/Produto.interface';
 import { environment } from 'src/environments/environment';
 import { Venda } from '../admin/vendas-admin/vendas.interface';
@@ -15,32 +16,45 @@ export class ConfirmarCompraService {
     private http: HttpClient
   ) { }
 
-  confirmarCompraTotal(listaProdutos: Produto[]) {
-
+  confirmarCompraTotal(listaProdutos: Produto[], callback: Function) {
     const baseUrl = environment.API + '/v1';
-    let itensVendaCriados: Observable<ItensVenda>[] = [];
-    let venda$: Observable<Venda> = new Subject<Venda>().asObservable();;
-
-    listaProdutos.forEach(item => {
+    let itensVendaCriados: Observable<number>[] = [];
+    
+    listaProdutos.forEach((item) => {
       const produtoID = item.id;
       const quantidade = 1; // TODO: quantidade fixa em 1 unidade.
+      
+      itensVendaCriados.push(
+        this.http
+        .post<ItensVenda>(`${baseUrl}/itens`, {
+          produtoID,
+          quantidade,
+        })
+        .pipe(map((itemVenda) => itemVenda.id))
+        );
+      });
+      
+      const formaPagamentoID = 1; // TODO: forma de pagamento fixa em 1
+      const clienteID = 16; // TODO: cliente fixa em 1
+      forkJoin(itensVendaCriados).subscribe(
+        itensVenda => {
+          console.log({ clienteID, formaPagamentoID, items: itensVenda });
+          this.http
+            .post<Venda>(`${baseUrl}/vendas`, {
+              clienteID,
+              formaPagamentoID,
+              items: itensVenda,
+            })
+            .subscribe(
+              (venda) => {
+                console.log(`Venda realizada: ID #${venda.id}`);
+                callback();
+              },
+              (err) => console.error
+            );
+      },
+      (err) => console.error
+    );
 
-      itensVendaCriados.push(this.http.post<ItensVenda>(`${baseUrl}/itens`, {produtoID, quantidade }))
-    });
-
-    forkJoin(itensVendaCriados).subscribe(
-      (itensVenda) => {
-      const clienteID = 16; // TODO: cliente fixo
-      const formaPagamentoID = 1; // TODO: forma de pagamento fixa
-      const items = itensVenda.map(item => item.id);
-
-      return venda$ = this.http.post<Venda>(`${baseUrl}/vendas`, {clienteID, formaPagamentoID, items})
-    }, 
-    (err) => {
-      console.log('Erro ao criar os itens de venda.');
-      console.error(err);
-    });
-
-    return venda$;
   }
 }
